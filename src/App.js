03fbd9e23581ae3bec20a1446d7d44be58f8a7d5
HwnaukLee <hwanuk.lee@gmail.com>
2020-08-25 17:54:51 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customers'
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동1',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동2',
+    'birthday': '980123',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길동3',
+    'birthday': '890709',
+    'gender': '남자',
+    'job': '대학생'
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <h2> 한자앱 </h2>
+      <div> {customers.map(c => { 
+              return( 
+                <Customer 
+                  key = {c.id} id={c.id} image={c.image} name = {c.name}
+                  birthday={c.birthday} gender={c.gender} job = {c.job}
+                /> 
+              );
+            }
+          )
+        }
       </div>
-    )
+    );
   }
 }
 
