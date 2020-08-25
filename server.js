0921170e3678de706cc50899edb0e6b23967b1bd
HwnaukLee <hwanuk.lee@gmail.com>
2020-08-26 00:20:43 +0900

    Add Get Customers REST API

--- server.js
+++ server.js
@@ -6,9 +6,41 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!'})
+
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동1',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
+          },
+        
+          {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '홍길동2',
+            'birthday': '980123',
+            'gender': '남자',
+            'job': '대학생'
+          },
+        
+          {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '홍길동3',
+            'birthday': '890709',
+            'gender': '남자',
+            'job': '대학생'
+          }
+    ])
 } );
+
+/* app.get('/api/hello', (req, res) => {
+    res.send({ message: 'Hello Express!'})
+} ); */
 
 
 /* const fs = require('fs');
