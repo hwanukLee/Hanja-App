ff4045d6f1ad7928d29386b39820b4a08c2e735c
HwnaukLee <hwanuk.lee@gmail.com>
2020-09-02 15:44:58 +0900

    Modal Insert/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { post } from 'axios';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/Styles';
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
+
 class CustomerAdd extends React.Component {
     
     constructor(props) {
@@ -11,7 +25,9 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileNmae: ''
+            fileNmae: '',
+            //Modal 기능 추가 위해 
+            open: false
         }
     }
     
@@ -30,7 +46,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileNmae: ''
+            fileNmae: '',
+            open: false
         })
         //this.props.stateRefresh();
         //window.location.reload();
@@ -68,8 +85,54 @@ class CustomerAdd extends React.Component {
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileNmae: '',
+            open: false
+        })
+    }
+
     render () {
+        const { classes } = this.props;
         return (
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle> 고객추가 </DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileNmae} onChange={this.handleFileChange} /><br/>
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileNmae === "" ? "프로필 이미지 선택": this.state.fileNmae}
+                            </Button>
+                        </label>
+                        <br />
+                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                        <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
+                        <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
+                        <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+
+            /*
             <form onSubmit = {this.handleFormSubmit}>
                 <h1>고객추가</h1>
                 프로필이미지: 
@@ -89,8 +152,10 @@ class CustomerAdd extends React.Component {
                     onChange={this.handleValueChange} /><br/>
                 <button type="submit">추가하기</button>
             </form>
+            */
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
+//export default CustomerAdd;
